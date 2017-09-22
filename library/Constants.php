2c7a1358355fd8122e9f3a5d591424e8f54fa551
Sparrow <mel.e.grigg@gmail.com>
2017-09-22 02:48:46 -0500

    Moved table names to Properties

--- library/Constants.php
+++ library/Constants.php
@@ -2,9 +2,6 @@
 
     class Constants {
 
-        public static $USER_TABLE = "test_users";
-        public static $POST_TABLE = "test_posts";
-
         public static $INDEX_PAGE = "main_page.php";
         public static $LOGIN_PAGE = "login_form.php";
         public static $REGISTER_PAGE = "register_form.php";
